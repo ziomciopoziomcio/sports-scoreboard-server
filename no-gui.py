cfa7c2f18c738777eec2c059e3f9dfa2ba73ade7
ziomciopoziomcio <jedrzejnie@gmail.com>
2022-07-31 00:27:34 +0200

    Added socket connector
    Only for test

--- no-gui.py
+++ no-gui.py
@@ -1,10 +1,16 @@
 #import libraries
+import sys
+import socket
+import selectors
+import types
+from typing import final
+# setting the variables
 
-# port selector with int checker
-
-HOST = None
+HOST = ""
 check = False
+sel = selectors.DefaultSelector()
 
+# int checker
 while check != True:
     PORT = input("Enter the port in the range from 1024 to 65535: ")
     if PORT.isnumeric():
@@ -20,3 +26,54 @@ while check != True:
     else:
         print("The specified value is not a port")
         check = False
+
+
+# socket connector
+
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((HOST, PORT))
+lsock.listen()
+print(f"Listening on {(HOST, PORT)}")
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+
+def accept_wrapper(sock):
+    conn, addr = sock.accept()
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024) 
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print(f"Closing connection to {data.addr}")
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"Echoing {data.outb!r} to {data.addr}")
+            sent = sock.send(data.outb)  
+            data.outb = data.outb[sent:]
+
+
+try:
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key, mask)
+except KeyboardInterrupt:
+    print("Caught keyboard interrupt, exiting")
+finally:
+    sel.close()
